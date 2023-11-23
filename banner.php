301887463374b326a59b8d67e186b0bc92daa70d
JustAmaldic <39012307+JustAmaldic@users.noreply.github.com>
2023-11-23 20:01:54 +0100

    banner

--- banner.php
+++ banner.php
@@ -45,33 +45,65 @@
         }
     </style>
     <?php
-    $bannerimg = [
-        "compok",
-        "vrok",
-        "game",
-        "monitor",
-        "Naamloos",
-        "nerdok",
-        "speak",
-        "toyok",
-        "vrok"
-    ]
+    include 'dbConnction.php';
+    $userid = 5;
+    $limit = 10;
+    $sql = "SELECT *, COUNT(*) AS order_count 
+
+FROM nerdy_gadgets_start.order O 
+
+JOIN user U ON U.id = O.user_id 
+
+JOIN order_item OI ON OI.order_id = O.id 
+
+JOIN Product P ON P.id = OI.product_id 
+
+WHERE U.id = $userid
+
+GROUP BY O.id, category, U.id, U.email 
+
+ORDER BY order_count desc 
+
+LIMIT 1;";
+    $result = mysqli_query($conn, $sql);
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
     ?>
+
+
 </head>
 
 <body>
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
     <?php
-    for($i = 0; $i < count($bannerimg); $i++){
-        print(
-        "<div class=\"swiper-slide\">
-            <a href=\"search.html\"> "
-        );
-        print ("<img src=\"banner/$bannerimg[$i].jpg\">");
-        print("</a>
+    for($i = 0; $i < count($rows); $i++){
+        foreach($rows[$i] as $rij => $waarde){
+            if($rij == 'category'){
+                $category = $waarde;
+                print(
+                "<div class=\"swiper-slide\">
+            <a href=\"search.html\"><img src=\"banner/images/$category.jpg\"></a>
         </div>");
+            }
+        }
     }
+//    $category = 'routers';
+    $sql2 = "SELECT * FROM Product WHERE category = '$category' LIMIT 5";
+    $result2 = mysqli_query($conn, $sql2);
+    $rows2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+    for($i = 0; $i < count($rows2); $i++){
+        foreach($rows2[$i] as $rij2 => $waarde3){
+            if($rij2 == 'image'){
+                print(
+                "<div class=\"swiper-slide\">
+    <a href=\"search.html\">
+    <img src=\"banner/images/$waarde3.jpg\">
+    </a>
+</div>");
+            }
+        }
+    }
+
     ?>
 
     </div>
