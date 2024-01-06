1f8703e61a2b0ad7bee7348579c5b3088e53be65
hamoedi <samoedi@hotmail.com>
2024-01-06 23:12:42 +0100

    product pagina html css

--- js/imageenlarger.js
+++ js/imageenlarger.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
         let selectedAdress;
         if (naam === "blauw1") {
            console.log(321)
-            selectedAdress = "foto's/refurb-iphone-13-blue-2023.jpeg"
+            selectedAdress = "foto's/refurb-iphone-13-blue-2023-removebg-preview.png"
         } else if (naam === "starlight1") {
-            selectedAdress = "foto's/refurb-iphone-13-starlight-2023.jpeg"
+            selectedAdress = "foto's/refurb-iphone-13-starlight-2023-removebg-preview.png"
         } else if (naam === "groen1") {
-            selectedAdress = "foto's/refurb-iphone-13-pro-green-2023.jpeg"
+            selectedAdress = "foto's/refurb-iphone-13-pro-green-2023-removebg-preview.png"
         } else if (naam === "midnight1") {
             selectedAdress = "foto's/iiPhone-13-Zwart-Middernacht-Achterkant-mobisite.png"
         }
@@ -27,4 +27,32 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("midnight1").addEventListener('click', function() {
         imageEnlargerFunction("midnight1");
     });
+    document.querySelector('.favorieten-button').addEventListener('click', function () {
+        playEasterEgg();
+    });
+
+    function playEasterEgg() {
+        // Grappige easter egg code
+        const body = document.querySelector('body');
+        const randomColor = getRandomColor();
+
+        body.style.transition = 'color 1s ease'; //
+        body.style.color = randomColor;
+
+        setTimeout(() => {
+            body.style.color = '';
+            body.style.transition = '';
+        }, 10000);
+
+    }
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
 });
