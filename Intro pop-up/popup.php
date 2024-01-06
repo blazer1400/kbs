3b52f5fb43ea01f6c059c6871328584abd377da4
JustAmaldic <39012307+JustAmaldic@users.noreply.github.com>
2024-01-06 16:22:03 +0100

    intro popup laatste veranderingen

--- Intro pop-up/popup.php
+++ Intro pop-up/popup.php
@@ -9,22 +9,41 @@
 </head>
 <body>
 <?php
-$userid = "Guest";
-if($userid !== "Guest"){
-    $radiosql = "FROM user SELECT popup WHERE id = $userid";
+include 'dbConn.php';
+include 'config.php';
+if(is_numeric($userid)){
+    $radiosql = "SELECT popup FROM user  WHERE id = $userid";
     $radioresult = mysqli_query($conn, $radiosql);
+    $yesornull = mysqli_fetch_all($radioresult, MYSQLI_ASSOC);
+for($i = 0; $i < count($yesornull); $i++){
+    $radio = $yesornull[$i]['popup'];
+    if($radio == 'Yes'){
+        } else {
+        print ("<div class=\"pop-up display\" id=\"popUp\">
+    <h1>Welcome to NerdyGadgets</h1>
+    <p>Bij Nerdy Gadgets geloven we in de kracht van technologie en popcultuur om mensen te verbinden en hun verbeelding te prikkelen. Onze missie is om de wereld van nerdy gadgets en verzamelobjecten toegankelijk te maken voor iedereen, zowel in onze fysieke winkel als in de digitale ruimte. We streven naar het bieden van hoogwaardige producten die de passies van techliefhebbers en popcultuurfanaten stimuleren. Onze waarden omvatten innovatie, nieuwsgierigheid, authenticiteit en community. We zijn vastbesloten om een brug te slaan tussen het verleden, heden en de toekomst van technologie en popcultuur, en streven ernaar om onze klanten een buitengewone ervaring te bieden, zowel online als offline. Welkom in de wereld van Nerdy Gadgets, waar jouw passies tot leven komen!</p>
+    <form id=\"myForm\">
+        <input type=\"radio\" checked id=\"radiocheck\" value=\"Yes\" style=\"display: none\" name=\"radio\">
+        <label for=\"radiocheck\" style=\"display: none\"></label>
+    </form>
+    <button class=\"pop-weg\" onclick=\"submitFormAndClose()\">OK</button>
+</div>");
+        }
+    }
+} else {
+    print ("<div class=\"pop-up display\" id=\"popUp\">
+    <h1>Welcome to NerdyGadgets</h1>
+    <p>Bij Nerdy Gadgets geloven we in de kracht van technologie en popcultuur om mensen te verbinden en hun verbeelding te prikkelen. Onze missie is om de wereld van nerdy gadgets en verzamelobjecten toegankelijk te maken voor iedereen, zowel in onze fysieke winkel als in de digitale ruimte. We streven naar het bieden van hoogwaardige producten die de passies van techliefhebbers en popcultuurfanaten stimuleren. Onze waarden omvatten innovatie, nieuwsgierigheid, authenticiteit en community. We zijn vastbesloten om een brug te slaan tussen het verleden, heden en de toekomst van technologie en popcultuur, en streven ernaar om onze klanten een buitengewone ervaring te bieden, zowel online als offline. Welkom in de wereld van Nerdy Gadgets, waar jouw passies tot leven komen!</p>
+    <form id=\"myForm\">
+        <input type=\"radio\" checked id=\"radiocheck\" value=\"Yes\" style=\"display: none\" name=\"radio\">
+        <label for=\"radiocheck\" style=\"display: none\"></label>
+    </form>
+    <button class=\"pop-weg\" onclick=\"submitFormAndClose()\">OK</button>
+</div>");
 }
 
 ?>
-<div class="pop-up display" id="popUp">
-    <h1>Welcome to NerdyGadgets</h1>
-    <p>Bij Nerdy Gadgets geloven we in de kracht van technologie en popcultuur om mensen te verbinden en hun verbeelding te prikkelen. Onze missie is om de wereld van nerdy gadgets en verzamelobjecten toegankelijk te maken voor iedereen, zowel in onze fysieke winkel als in de digitale ruimte. We streven naar het bieden van hoogwaardige producten die de passies van techliefhebbers en popcultuurfanaten stimuleren. Onze waarden omvatten innovatie, nieuwsgierigheid, authenticiteit en community. We zijn vastbesloten om een brug te slaan tussen het verleden, heden en de toekomst van technologie en popcultuur, en streven ernaar om onze klanten een buitengewone ervaring te bieden, zowel online als offline. Welkom in de wereld van Nerdy Gadgets, waar jouw passies tot leven komen!</p>
-    <form id="myForm">
-        <input type="radio" checked id="radiocheck" value="Yes" style="display: none" name="radio">
-        <label for="radiocheck" style="display: none"></label>
-    </form>
-    <button class="pop-weg" onclick="submitFormAndClose()">OK</button>
-</div>
+
 
 <script>
     function submitFormAndClose() {
--- Intro pop-up/process_popup.php
+++ Intro pop-up/process_popup.php
@@ -1,19 +1,17 @@
 <?php
-include 'dbConnction.php';
+include 'dbConn.php';
+include 'config.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $radiocheck = $_POST['radio'];
 
-    $usedb = "USE nerdy_gadgets_start";
-    mysqli_query($conn, $usedb);
-    $newcolumn = "ALTER TABLE user
-ADD COLUMN popup VARCHAR(255) DEFAULT NULL;";
-    mysqli_query($conn, $newcolumn);
+if(is_numeric($userid)){
     if(isset($_POST['radio'])){
         $newrow = "UPDATE user
 SET popup = 'Yes'
-WHERE id = 1;";
+WHERE id = $userid;";
         mysqli_query($conn, $newrow);
     }
+}
     $response = ["status" => "success", "message" => "Welcome to NerdyGadgets"];
     echo json_encode($response);
     error_log("Form data: " . print_r($_POST, true));
