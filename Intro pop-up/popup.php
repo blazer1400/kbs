aab68af04326f5e96d0b24bc795b65c26f9ace61
JustAmaldic <39012307+JustAmaldic@users.noreply.github.com>
2024-01-07 16:44:22 +0100

    Popup bevat nu de slogan erbij

--- Intro pop-up/popup.php
+++ Intro pop-up/popup.php
@@ -36,6 +36,8 @@ for($i = 0; $i < count($yesornull); $i++){
     print ("<div class=\"pop-up display\" id=\"popUp\">
     <h1>Welcome to NerdyGadgets</h1>
     <p>Bij Nerdy Gadgets geloven we in de kracht van technologie en popcultuur om mensen te verbinden en hun verbeelding te prikkelen. Onze missie is om de wereld van nerdy gadgets en verzamelobjecten toegankelijk te maken voor iedereen, zowel in onze fysieke winkel als in de digitale ruimte. We streven naar het bieden van hoogwaardige producten die de passies van techliefhebbers en popcultuurfanaten stimuleren. Onze waarden omvatten innovatie, nieuwsgierigheid, authenticiteit en community. We zijn vastbesloten om een brug te slaan tussen het verleden, heden en de toekomst van technologie en popcultuur, en streven ernaar om onze klanten een buitengewone ervaring te bieden, zowel online als offline. Welkom in de wereld van Nerdy Gadgets, waar jouw passies tot leven komen!</p>
+    <br>
+    <p>-Nerdy Gadgets, slimme prijzen.</p> 
     <form id=\"myForm\">
         <input type=\"radio\" checked id=\"radiocheck\" value=\"Yes\" style=\"display: none\" name=\"radio\">
         <label for=\"radiocheck\" style=\"display: none\"></label>
