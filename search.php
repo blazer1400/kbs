45b73d7f6a1c0bca3d9ef86f9db94abf60160935
Stan Broersma <sbroersma@roc-dev.com>
2024-01-07 23:46:49 +0100

    search fixed

--- search.php
+++ search.php
@@ -4,40 +4,37 @@
     <meta charset="UTF-8">
     <title>KBS</title>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <link rel="stylesheet" href="css/search.css" />
+    <link rel="stylesheet" href="css/search.css"/>
 </head>
 <body>
 
-    <?php include 'header.php';    ?>
-    <?php
-    $resultCategory = [];
+<?php include 'header.php'; ?>
+<?php
+$resultCategory = [];
 
-    ?>
+?>
 
 <div class="row">
     <div class="column side-left">
         <div class="filter-section">
             <h3>Filters</h3>
-
-
-            <hr>
-            <ul class="filter-options">
-                <li>
-                    <a href="#">Prijs</a>
-                    <label>
-                        <input>
-                    </label>
-                    <label>
-                        <input>
-                    </label>
-
-                </li>
-                <li>
-                    <form action="" method="get">
+            <form action="" method="post">
+                <hr>
+                <ul class="filter-options">
                     <div class="category">
-                        <a href="#">Category</a>
+                        <p href="#">Category</p>
                         <?php
-                        include('./php/db_connection.php');
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "nerdy_gadgets";
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
 
                         // QUERY
                         $sql = "SELECT DISTINCT category FROM product";
@@ -47,88 +44,117 @@
                         if ($result->num_rows > 0) {
                             // PRODUCT RASTER
                             echo '<div class="product-raster">';
-                            $connection = $conn;
+                            $connection = mysqli_connect('127.0.0.1', 'root', '', 'nerdy_gadgets', '3306');
                             $sql_selectALL = "SELECT * FROM product";
                             $res = mysqli_query($connection, $sql);
-
+                            $sqlCategorys = [];
                             while ($row = mysqli_fetch_assoc($res)) {
                                 $productCategory = $row["category"];
                                 $resultCategory[] = $productCategory;
-                                echo "<label>";
-                                echo "<input type='checkbox' " . (isset($_GET[$productCategory]) ? "checked='checked'" : "") . " onchange='this.form.submit()' name='$productCategory'>$productCategory <br>";
-                                echo "</label>";
 
+                                echo "<label>";
+                                echo "<input type='checkbox' " . (isset($_POST[$productCategory])  ? "checked='checked'" : "") . (isset($_POST[$productCategory])  ? ($sqlCategorys[] = "$productCategory") : "")  .  " onchange='this.form.submit()' name='$productCategory'>$productCategory <br>";
+                                echo "</label>";
                             }
+                            mysqli_close($connection);
                             echo "</div>";
                         } else {
                             echo "0 results";
 
                         }
+                        $conn->close();
                         ?>
+                        <hr>
+                        <li>
+                            <p href="#">Prijs</p>
+                            <div class="prijsInput">
+                                <label>
+                                    <input type="number" class="inputPrijs" name="prijs-van" placeholder="van"
+                                        <?php
+                                        if (isset($_POST["prijs-van"]))
+                                            echo 'value = "' . $_POST["prijs-van"] . '"'; ?>>
+                                </label>
+                                <p>t/m</p>
+                                <label>
+                                    <input type="number" class="inputPrijs" name="prijs-tot" placeholder="tot"
+                                        <?php
+                                        if (isset($_POST["prijs-tot"]))
+                                            echo 'value = "' . $_POST["prijs-tot"] . '"'; ?>>
+                                </label>
+                                <br>
+
+                            </div>
+                            <label>
+                                <input type="submit" class="submit-button" value="Toepassen">
+                            </label>
+                        </li>
+
                     </div>
-                    </form>
-                </li>
-            </ul>
+
+
+                </ul>
+            </form>
         </div>
     </div>
 
     <div class="column middle">
         <div class="content-wrapper">
             <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "nerdy_gadgets";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            // echo "Connected successfully<br>";
 
 
             // QUERY
-            $sqlConditite = "";
-            $sqlCategorys = [];
 
-            if(isset($_GET["$resultCategory[0]"])){
-                $sqlCategorys[] = "laptops";
-            }
-            if(isset($_GET[$resultCategory[1]])){
-                $sqlCategorys[] = "phones";
-            }
-            if(isset($_GET[$resultCategory[2]])){
-                $sqlCategorys[] = "opslag";
-            }
-            if(isset($_GET[$resultCategory[3]])){
-                $sqlCategorys[] = "routers";
-            }
-            if(isset($_GET[$resultCategory[4]])){
-                $sqlCategorys[] = "componenten";
-            }
-            if(isset($_GET[$resultCategory[5]])){
-                $sqlCategorys[] = "desktops";
-            }
+            // ... (your existing code)
 
-            if(count($sqlCategorys) == 1){
+            // QUERY
+            $sql_query = "SELECT * FROM product WHERE 1"; // Initial query
+
+            if (count($sqlCategorys) == 1) {
                 $sqlStatement = $sqlCategorys[0];
-                $sqlConditite = "WHERE category = '$sqlStatement'";
-                $sql = "SELECT * FROM product $sqlConditite";
-            }else if(count($sqlCategorys ) > 1){
-                foreach ($sqlCategorys as $yuh){
-                    echo "";
+                $sqlConditite = " AND category = '$sqlStatement'";
+                $sql_query .= $sqlConditite;
+            } else if (count($sqlCategorys) > 1) {
+                $categories = implode("','", $sqlCategorys);
+                $sql_query .= " AND category IN ('$categories')";
+            }
+
+            // Check if price range is provided
+            if (isset($_POST['prijs-van']) && isset($_POST['prijs-tot']))  {
+                $prijsVan = $_POST['prijs-van'];
+                $prijsTot = $_POST['prijs-tot'];
+                if ($prijsTot == 404 || $prijsVan == 404){
+                    echo "<h1 class='zero-results'>0 results</h1>";
+                    return;
                 }
-                $sql = "SELECT * FROM product";
-            }else{
-                $sql = "SELECT * FROM product";
+                else if ($prijsVan !== '' && $prijsTot !== '') {
+                    $sql_query .= " AND price BETWEEN $prijsVan AND $prijsTot";
+                }
+            }
+
+            if (isset($_GET['q'])) {
+                $sql_query .= " AND name LIKE '%" . $_GET['q'] . "%'";
             }
 
             // RESULT
-            $result = $conn->query($sql);
+            $result = $conn->query($sql_query);
 
-            if ($result && $result->num_rows > 0) {
+            if ($result->num_rows > 0) {
                 // PRODUCT RASTER
                 echo '<div class="product-raster">';
-                $connection = $conn;
-                if (isset($_GET['q'])) {
-                    $sql = $sql . " WHERE name LIKE '%" . $_GET['q'] . "%'";
-                }
-                $res = $conn->query($sql);
-
-                print_r($conn->error);
-
-
-                while ($row = $res->fetch_assoc()) {
+                // Use the existing connection object instead of creating a new one
+                while ($row = $result->fetch_assoc()) {
                     $productID = $row["id"];
                     $productImage = $row["image"];
                     $productName = $row["name"];
@@ -138,41 +164,28 @@
 
                     echo "<a href='product.php?id=$productID'>";
                     echo "<div class='content-box'>";
-//                    echo "<div class='content-category'>$productCategory;</div>";
                     echo "<h3 class='content-name'>$productName</h3>";
-                    echo "<div class='content-img'><img src ='img/products/$productImage.jpg' alt='$productID'></div>";
+                    echo "<div class='content-img'><img src ='img/product_images/$productImage.jpg' alt='$productID'></div>";
                     echo "<p class='content-price'>€$productPrice</p>";
                     echo "</div>";
                     echo "</a>";
                 }
-                mysqli_close($connection);
                 echo "</div>";
             } else {
-                echo "0 results";
+                echo "<h1 class='zero-results'>0 results</h1>";
             }
+            $conn->close();
             ?>
 
         </div>
-
-<!--        <div class="show-more-section">-->
-<!--            <button class="show-more-button">Show more</button>-->
-<!--        </div>-->
-        </div>
-
-    <div class="column side-right">
-        <div class="feedback">
-
-        </div>
     </div>
-
-
 </div>
 
 
-
-<div >
+<div>
     <?php include 'footer.php';
     ?>
 </div>
+
 </body>
 </html>
